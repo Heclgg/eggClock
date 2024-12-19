ae9ca3c2d8bfd011f3c7c3f5b25190b994e2703f
Heclgg <heclgg@foxmail.com>
2024-12-19 13:45:37 +0800

    隐藏系统导航栏

--- app/src/main/java/org/heclgg/eggclock/ClockMainActivity.kt
+++ app/src/main/java/org/heclgg/eggclock/ClockMainActivity.kt
@@ -2,6 +2,9 @@ package org.heclgg.eggclock
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 /**
  * 桌面时钟主页面
@@ -13,6 +16,17 @@ class ClockMainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBar()
         setContentView(R.layout.clock_main)
     }
+
+    /**
+     * 隐藏系统状态栏和导航栏
+     */
+    private fun hideSystemBar() =
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
 }
