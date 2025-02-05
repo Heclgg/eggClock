b76f64312e942be6af91a5eee6fa902a80db0fd0
Heclgg <heclgg@foxmail.com>
2025-02-05 21:18:10 +0800

    字体变大

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.301"
+        versionName = "1.0.302"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
